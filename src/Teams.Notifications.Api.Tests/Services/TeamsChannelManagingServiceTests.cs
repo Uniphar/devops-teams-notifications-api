a18b6a4a0de5f685e51e2b013e46b81bde3fcd5e
Daniël Koek <contact@danielkoek.net>
2025-04-14 13:28:45 +0100

    basic

---
+++ src/Teams.Notifications.Api.Tests/Services/TeamsChannelManagingServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teams.Notifications.Api.Tests.Services
+{
+    [TestClass]
+    [TestCategory("Integration")]
+    public sealed class TeamsChannelManagingServiceTests
+    {
+        private static DefaultAzureCredential _defaultAzureCredential;
+        private static CancellationToken _cancellationToken;
+
+        [ClassInitialize]
+        public static async Task ClassInitialize(TestContext context)
+        {
+            var env = context.Properties["Environment"]!.ToString();
+            _cancellationToken = context.CancellationTokenSource.Token;
+            _defaultAzureCredential = new DefaultAzureCredential();
+        }
+    }
+}
