7c8e3b49b3cea17f413bd8227abcd70c004bcae4
Daniël Koek <contact@danielkoek.net>
2025-05-07 08:57:08 +0100

    Update TelemetryProcessor.cs

--- src/Teams.Notifications.Api/Telemetry/TelemetryProcessor.cs
+++ src/Teams.Notifications.Api/Telemetry/TelemetryProcessor.cs
@@ -1,27 +1,19 @@
-﻿using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.ApplicationInsights.Extensibility;
-
-namespace Teams.Notifications.Api.Telemetry;
+﻿namespace Teams.Notifications.Api.Telemetry;
 
 public class TelemetryProcessor(ITelemetryProcessor next) : ITelemetryProcessor
 {
-	public void Process(ITelemetry item)
-	{
-		if (item is not ISupportProperties telemetryItem)
-		{
-			next.Process(item);
-			return;
-		}
+    public void Process(ITelemetry item)
+    {
+        if (item is not ISupportProperties telemetryItem)
+        {
+            next.Process(item);
+            return;
+        }
 
-		if (telemetryItem.Properties.TryGetValue(HttpContextTelemetryInitializer.RequestPath, out string? requestPath))
-		{
-			if (requestPath.EndsWith("/health"))
-			{
-				return;
-			}
-		}
+        if (telemetryItem.Properties.TryGetValue(HttpContextTelemetryInitializer.RequestPath, out var requestPath))
+            if (requestPath.EndsWith("/health"))
+                return;
 
-		next.Process(item);
-	}
+        next.Process(item);
+    }
 }
