91972764d44847f4893ac6dfbdc470e4c7906f7e
Daniël Koek <contact@danielkoek.net>
2025-05-20 10:30:43 +0100

    More stuff added!

--- src/Teams.Notifications.Api/Services/Interfaces/ICardManagerService.cs
+++ src/Teams.Notifications.Api/Services/Interfaces/ICardManagerService.cs
@@ -2,7 +2,7 @@
 
 public interface ICardManagerService
 {
-    Task CreateCard(string jsonFileName, CardTemplateModel model, string teamId, string channelId);
-    Task UpdateCard(string jsonFileName, string uniqueId, CardTemplateModel model, string teamId, string channelId);
+    Task CreateCard(string jsonFileName, object model, string teamId, string channelId);
+    Task UpdateCard(string jsonFileName, string uniqueId, object model, string teamId, string channelId);
     Task DeleteCard(string jsonFileName, string uniqueId, string teamId, string channelId);
 }
