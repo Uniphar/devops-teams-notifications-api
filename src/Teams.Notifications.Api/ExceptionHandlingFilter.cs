f5d2f8dd01a6b2cfe74bc09a4dea7fc464358fda
Daniël Koek <contact@danielkoek.net>
2025-08-28 14:14:09 +0100

    Track the exceptions right

--- src/Teams.Notifications.Api/ExceptionHandlingFilter.cs
+++ src/Teams.Notifications.Api/ExceptionHandlingFilter.cs
@@ -2,16 +2,30 @@
 
 internal class ExceptionHandlingFilter : IExceptionFilter
 {
+    private readonly TelemetryClient _telemetry;
+
+    public ExceptionHandlingFilter(TelemetryClient telemetry)
+    {
+        _telemetry = telemetry;
+    }
     public void OnException(ExceptionContext context)
     {
-        context.Result = context.Exception switch
+        switch (context.Exception)
         {
-            NotImplementedException => new StatusCodeResult(StatusCodes.Status501NotImplemented),
+            case NotImplementedException:
+                _telemetry.TrackException(context.Exception);
+                context.Result = new StatusCodeResult(StatusCodes.Status501NotImplemented);
+                break;
             // any of the InvalidOperationException is considered a user messes up action, so we can do a 400 with the info
-            InvalidOperationException invalidOperation =>
-                new ObjectResult(invalidOperation.Message) { StatusCode = StatusCodes.Status400BadRequest },
-            // do nothing, let the default handler deal with it
-            _ => context.Result
-        };
+            case InvalidOperationException invalidOperation:
+                _telemetry.TrackException(context.Exception);
+                context.Result = new ObjectResult(invalidOperation.Message) { StatusCode = StatusCodes.Status400BadRequest };
+                break;
+            default:
+                _telemetry.TrackException(context.Exception);
+                // do nothing, let the default handler deal with it
+                context.Result = context.Result;
+                break;
+        }
     }
 }
