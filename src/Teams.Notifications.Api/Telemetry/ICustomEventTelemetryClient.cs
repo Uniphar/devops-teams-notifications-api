110af328b405079cf0b713d4b96959256c585605
Daniël Koek <contact@danielkoek.net>
2025-08-29 15:27:25 +0100

    and recursive

--- src/Teams.Notifications.Api/Telemetry/ICustomEventTelemetryClient.cs
+++ src/Teams.Notifications.Api/Telemetry/ICustomEventTelemetryClient.cs
@@ -3,6 +3,7 @@
 public interface ICustomEventTelemetryClient
 {
     void TrackEvent(string eventName, object properties);
+    void TrackEvent(string eventName);
     void TrackException(Exception ex, object state);
     void TrackException(Exception ex);
 }
