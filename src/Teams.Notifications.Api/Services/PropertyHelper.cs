259b7d53e2b114cb727f2a2ff6e889c00cde3550
Daniël Koek <contact@danielkoek.net>
2025-08-29 15:21:41 +0100

    much cleaner!

--- src/Teams.Notifications.Api/Services/PropertyHelper.cs
+++ src/Teams.Notifications.Api/Services/PropertyHelper.cs
@@ -23,7 +23,7 @@ public static class PropertyHelper
         {
             // optional string, will remove the block if empty
             case "string?":
-                var valueString = model.TryGetStringPropertyValue(property).ToJsonString();
+                var valueString = JsonEncodedText.Encode(model.TryGetStringPropertyValue(property)?? string.Empty).Value;
                 if (!string.IsNullOrEmpty(valueString)) return jsonString.Replace(toReplace, valueString);
                 // Parse JSON and remove objects from arrays where the property value matches the placeholder
                 var rootString = JsonNode.Parse(jsonString);
@@ -32,7 +32,7 @@ public static class PropertyHelper
 
             // required string
             case "string":
-                return jsonString.Replace(toReplace, model.TryGetStringPropertyValue(property).ToJsonString() ?? string.Empty);
+                return jsonString.Replace(toReplace, JsonEncodedText.Encode(model.TryGetStringPropertyValue(property) ?? string.Empty).Value);
             case "int":
                 return jsonString.Replace(toReplace, model.TryGetIntPropertyValue(property)?.ToString() ?? string.Empty);
             case "file":
