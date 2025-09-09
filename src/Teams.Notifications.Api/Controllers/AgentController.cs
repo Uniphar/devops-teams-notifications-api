c09b730c4e91e71afe7d027857abb20ea55387e2
Daniël Koek <contact@danielkoek.net>
2025-09-09 14:32:41 +0100

    More logging

--- src/Teams.Notifications.Api/Controllers/AgentController.cs
+++ src/Teams.Notifications.Api/Controllers/AgentController.cs
@@ -7,7 +7,48 @@
 [ApiController]
 public class AgentController : ControllerBase
 {
-    [HttpPost] [Authorize(AuthenticationSchemes = "AgentScheme")] [ApiExplorerSettings(IgnoreApi = true)] public Task PostMessages([FromServices] IAgentHttpAdapter adapter, [FromServices] IAgent agent, CancellationToken cancellationToken) => adapter.ProcessAsync(Request, Response, agent, cancellationToken);
+    private readonly ILogger<AgentController> _logger;
+    private readonly ICustomEventTelemetryClient _telemetry;
 
-    [HttpGet] [Authorize(AuthenticationSchemes = "AgentScheme")] [ApiExplorerSettings(IgnoreApi = true)] public Task GetMessages([FromServices] IAgentHttpAdapter adapter, [FromServices] IAgent agent, CancellationToken cancellationToken) => adapter.ProcessAsync(Request, Response, agent, cancellationToken);
+    public AgentController(ILogger<AgentController> logger, ICustomEventTelemetryClient telemetry)
+    {
+        _logger = logger;
+        _telemetry = telemetry;
+    }
+
+    [HttpPost]
+    [Authorize(AuthenticationSchemes = "AgentScheme")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public Task PostMessages([FromServices] IAgentHttpAdapter adapter, [FromServices] IAgent agent, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return adapter.ProcessAsync(Request, Response, agent, cancellationToken);
+        }
+
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error processing card action");
+            _telemetry.TrackException(ex);
+            throw;
+        }
+    }
+
+    [HttpGet]
+    [Authorize(AuthenticationSchemes = "AgentScheme")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public Task GetMessages([FromServices] IAgentHttpAdapter adapter, [FromServices] IAgent agent, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return adapter.ProcessAsync(Request, Response, agent, cancellationToken);
+        }
+
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error processing message");
+            _telemetry.TrackException(ex);
+            throw;
+        }
+    }
 }
