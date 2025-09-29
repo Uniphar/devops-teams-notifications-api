8ed7c5958bcb23a1ab4a7672e0e9cbe73092ca8c
Daniël Koek <contact@danielkoek.net>
2025-09-29 11:18:54 +0100

    comments

--- src/Teams.Notifications.Api/OpenapiTransformer/AddAdaptiveCardDocsTransformer.cs
+++ src/Teams.Notifications.Api/OpenapiTransformer/AddAdaptiveCardDocsTransformer.cs
@@ -34,18 +34,20 @@ public sealed class AddAdaptiveCardDocsTransformer : IOpenApiDocumentTransformer
     {
         var paths = document.Paths;
         foreach (var path in paths)
-        foreach (var operation in path.Value.Operations)
-            if (operation.Key == OperationType.Get)
-            {
-                var okResponse = operation.Value.Responses["200"];
-                var schema = okResponse.Content["application/json"].Schema;
-                schema.Type = "object";
-                schema.ExternalDocs = new OpenApiExternalDocs
+        {
+            foreach (var operation in path.Value.Operations)
+                if (operation.Key == OperationType.Get)
                 {
-                    Description = "Find out more about Adaptive Cards",
-                    Url = new Uri("https://adaptivecards.io/schemas/adaptive-card.json")
-                };
-            }
+                    var okResponse = operation.Value.Responses["200"];
+                    var schema = okResponse.Content["application/json"].Schema;
+                    schema.Type = "object";
+                    schema.ExternalDocs = new OpenApiExternalDocs
+                    {
+                        Description = "Find out more about Adaptive Cards",
+                        Url = new Uri("https://adaptivecards.io/schemas/adaptive-card.json")
+                    };
+                }
+        }
 
         return Task.CompletedTask;
     }
--- src/Teams.Notifications.Api/OpenapiTransformer/AddCorrectFileTransformer.cs
+++ src/Teams.Notifications.Api/OpenapiTransformer/AddCorrectFileTransformer.cs
@@ -19,27 +19,26 @@ public sealed class AddCorrectFileTransformer : IOpenApiOperationTransformer
         if (operation.RequestBody?.Content == null || !operation.RequestBody.Content.TryGetValue("multipart/form-data", out var value)) return Task.CompletedTask;
         if (value.Schema.Type == "object" && value.Schema.Properties.ContainsKey("file"))
             operation.RequestBody.Content["multipart/form-data"] = new OpenApiMediaType
+            {
+                Encoding = new Dictionary<string, OpenApiEncoding>
                 {
-                    Encoding = new Dictionary<string, OpenApiEncoding>
+                    ["file"] = new()
+                    {
+                        Style = ParameterStyle.Form
+                    }
+                },
+                Schema = new OpenApiSchema
+                {
+                    Properties = new Dictionary<string, OpenApiSchema>
                     {
                         ["file"] = new()
                         {
-                            Style = ParameterStyle.Form
-                        }
-                    },
-                    Schema = new OpenApiSchema
-                    {
-                        Properties = new Dictionary<string, OpenApiSchema>
-                        {
-                            ["file"] = new()
-                            {
-                                Type = "string",
-                                Format = "binary"
-                            }
+                            Type = "string",
+                            Format = "binary"
                         }
                     }
                 }
-                ;
+            };
         return Task.CompletedTask;
     }
 }
